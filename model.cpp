500868fece438c9d93826266476a4bf01293511c
kensyou-kun <pikka.tech@gmail.com>
2022-09-14 14:59:54 +0900

    hotfix : std::cout << "Hello, World" << std::endl; bool flag == true;int i = nullptr;int array_[3] = { 0, 0, 0 }; c.set(6, 30);

--- model.cpp
+++ model.cpp
@@ -9,3 +9,6 @@ void run(){}
 int main(){
 #include <vector>
 #include <string>
+std::cout << "Hello, World" << std::endl;
+bool flag == true;int i = nullptr;int array_[3] = { 0, 0, 0 };
+c.set(6, 30);
