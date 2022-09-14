f072176f7fa950db918a51ec15f42575600a3dcf
kensyou-kun <pikka.tech@gmail.com>
2022-09-14 15:14:09 +0900

    fix : bool flag == true;int i = nullptr;int array_[3] = { 0, 0, 0 }; printf("%02d:%02d ", hour, minute); #include <stdio>

--- manager.cpp
+++ manager.cpp
@@ -134,3 +134,7 @@ public:
 #include <vector>
 printf("%02d:%02d
 ", hour, minute);
+bool flag == true;int i = nullptr;int array_[3] = { 0, 0, 0 };
+printf("%02d:%02d
+", hour, minute);
+#include <stdio>
