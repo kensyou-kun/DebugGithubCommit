8d29cd26ceafcda25a7780b70dc2d3f57fd115a5
kensyou-kun <pikka.tech@gmail.com>
2022-09-14 15:08:53 +0900

    add : #include <string> template <typename T1, typename T2> this->hour = hour % 24;

--- model.cpp
+++ model.cpp
@@ -68,3 +68,6 @@ ptr_array = array_;std::cout << "ptr_array " << ptr_array << std::endl;
 this->hour = hour % 24;
 #include <stdio>
 void run(){}
+#include <string>
+template <typename T1, typename T2>
+this->hour = hour % 24;
