4831d78b4f5a1f381df5358e1bfe2dcb25b4e8c5
kensyou-kun <pikka.tech@gmail.com>
2022-09-14 14:14:18 +0900

    fix

---
+++ run.py
@@ -0,0 +1,25 @@
+import os
+import random
+import string
+import subprocess
+
+commit_message = ["add","edit","fix","hotfix","hack","merge"]
+
+commit_num = 10
+
+def MakeCommit():
+    files = os.listdir()
+    files.remove(os.path.basename(__file__))
+
+    edit = random.choice(files)
+
+    rand = random.choice(string.ascii_letters + string.digits)
+
+    with open(edit, 'w') as f:
+        f.write(rand)
+
+    subprocess.run(["git", "add","."])
+    subprocess.run(["git", "commit","-m",random.choice(commit_message)+rand])
+
+if __name__ == "__main__": 
+    MakeCommit()
