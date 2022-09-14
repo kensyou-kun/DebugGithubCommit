d61701190a15133d623a62413d6aa793d2d550b9
kensyou-kun <pikka.tech@gmail.com>
2022-09-14 15:06:43 +0900

    hack : printf("%02d:%02d ", hour, minute); #include <stdio> #include <vector>

--- mesh.cpp
+++ mesh.cpp
@@ -93,3 +93,7 @@ printf("%02d:%02d
 bool flag == true;int i = nullptr;int array_[3] = { 0, 0, 0 };
 #include <stdio>
 this->hour = hour % 24;
+printf("%02d:%02d
+", hour, minute);
+#include <stdio>
+#include <vector>
